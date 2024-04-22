d3cf22ab264402c9bca2926aec8b95951520ab6c
nuenuey <youyy0206@jbnu@ac.kr>
2024-04-22 11:58:35 +0900

    4/22

--- lec12/calc_gdd.py
+++ lec12/calc_gdd.py
@@ -1,30 +1,26 @@
 from weather_stat import read_col, read_col_int
 #from weather_stat import read_col, read_col_int
 
-def gdd_season(tavg, months):
-    total_gdd=0
-
-    for temp, month in zip(tavg,months):
+def gdd_season(tavg_data, months):
+    gdd_sum = 0
+    for temp, month in zip(tavg_data, months):
         if month in [5, 6, 7, 8, 9]:
-            eff_temp =temp -5
+            eff_temp = temp - 5
             if eff_temp < 0:
-                eff_temp =0
-            total_gdd += eff_temp
-
-
-    #total_gdd=sum([temp -5 if temp > 5 else 0 for temp, month
-    #                in zip (tavg,months) if month in [5, 6, 7, 8, 9]  ])
-    return total_gdd
+                eff_temp = 0
+            gdd_sum += eff_temp
+    return gdd_sum
 
 
 
 def main():
-    filename="lec12/weather(146)_2022-2022 (1).csv"
-    tavg= read_col(filename,"tavg")
-    months= read_col_int(filename,"month")
-
+    
     print(f"GDD는 {gdd_season(tavg,months):.1f}도일입니다.")
+    weather_filename = "hw12/weather(146)_2001-2022 (1).csv"
+    weather_filename_22 = "hw12/weather(146)_2022-2022.csv"
 
+
+    tavg = read_col(weather_filename_22, "tavg")
     
 if __name__ == "__main__":
     main()
